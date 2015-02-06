5ddb73d7f858dbcbb256a53078ec659e4c992ce1
darthfett <darthfett@gmail.com>
2015-02-06 14:38:57 -0700

    Canvas auto-resizes according to window size

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
-var update_interval = 30;
+// CONST
+var UPDATE_INTERVAL = 1000/60;
+
+// GLOBALS
+var key_pressed = {};
+var canvas;
+var context;
+var i = 0;
+
+function on_keydown(ev) {
+    var key = ev.which;
+    key_pressed[key] = true;
+}
+
+function on_keyup(ev) {
+    var key = ev.which;
+    key_pressed[key] = false;
+}
 
 function draw() {
-    let canvas = document.getElementById("canvas");
-    let context = canvas.getContext("2d");
-    let interval_id = window.setInterval(function update() {
-        // Update canvas
-    }, update_interval);
-
-    window.onkeydown  = function(event) {
-        let key = event.which;
-    }
-    window.onkeyup = function(event) {
-        let key = event.which;
-    }
+    context.clearRect (0, 0, canvas.width, canvas.height);
+    
+    context.fillRect(i, i, 150, 75);
 }
+
+function on_update() {
+    i += 1;
+    if (i+150 > canvas.width || i+75 > canvas.height) {
+        i = 0;
+    }
+    draw();
+}
+
+function on_resize() {
+    context.canvas.width = window.innerWidth;
+    context.canvas.height = window.innerHeight;
+    context.fillStyle = "#FF0000";
+    draw();
+}
+
+function main() {
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext("2d");
+    var interval_id = window.setInterval(on_update, UPDATE_INTERVAL);
+    context.fillStyle = "#FF0000";
+
+    window.addEventListener('resize', on_resize);
+    window.onkeydown = on_keydown;
+    window.onkeyup = on_keyup;
+}
+
+document.addEventListener("DOMContentLoaded", main);
